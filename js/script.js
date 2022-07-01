1cb2cdb0cdf243a57b2cbab1b949b3a9ebc1313d
Matthew Faraz Tootoonchi <mftootoonchi@cpp.edu>
2022-07-01 12:38:10 -0700

    animations0

--- js/script.js
+++ js/script.js
@@ -9,6 +9,30 @@ scroll > 700 && scroll < 1551 ? document.getElementById("textLink0ID").setAttrib
 
 scroll > 1550 ? document.getElementById("textLink1ID").setAttribute('style', 'background-color: whitesmoke') : document.getElementById("textLink1ID").setAttribute('style', 'background-color: #444444')
 
+scroll > 420 ? document.getElementById("mainSection1H10ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection1H10ID").setAttribute('style', 'opacity: 0');
+
+scroll > 720 ? document.getElementById("mainSection1P0ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection1P0ID").setAttribute('style', 'opacity: 0');
+
+scroll > 1290 ? document.getElementById("mainSection2H10ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2H10ID").setAttribute('style', 'opacity: 0');
+
+scroll > 1580 ? document.getElementById("img1ID").setAttribute('style', 'opacity: 1') : document.getElementById("img1ID").setAttribute('style', 'opacity: 0');
+
+scroll > 1580 ? document.getElementById("mainSection2Main0H20ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main0H20ID").setAttribute('style', 'opacity: 0');
+
+scroll > 1580 ? document.getElementById("mainSection2Main0P0ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main0P0ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2160 ? document.getElementById("img2ID").setAttribute('style', 'opacity: 1') : document.getElementById("img2ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2160 ? document.getElementById("mainSection2Main1H20ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main1H20ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2160 ? document.getElementById("mainSection2Main1P0ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main1P0ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2730 ? document.getElementById("img3ID").setAttribute('style', 'opacity: 1') : document.getElementById("img3ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2730 ? document.getElementById("mainSection2Main2H20ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main2H20ID").setAttribute('style', 'opacity: 0');
+
+scroll > 2730 ? document.getElementById("mainSection2Main2P0ID").setAttribute('style', 'opacity: 1') : document.getElementById("mainSection2Main2P0ID").setAttribute('style', 'opacity: 0');
+
 console.log(scroll);
 
 });
